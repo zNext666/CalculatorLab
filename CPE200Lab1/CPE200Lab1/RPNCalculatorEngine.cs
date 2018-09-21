b06489b38878af786ce980e6ca85c08477df7412
zNext666 <alonereview@gmail.com>
2018-09-21 13:52:32 +0700

    Complete all test case

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -10,11 +10,23 @@ namespace CPE200Lab1
     {
         public new string Process(string str)
         {
+            //testing
+            //str = "+1";
+            if (str == null || str == "")
+            {
+                return "E";
+            }
             Stack<string> rpnStack = new Stack<string>();
             List<string> parts = str.Split(' ').ToList<string>();
             string result;
             string firstOperand, secondOperand;
-
+            //Console.WriteLine(Double.Parse(parts[0]));
+            /*
+            if (isOperator(parts[0]) && !isNumber(parts[0]))
+            {
+                return "E";
+            }
+            */
             foreach (string token in parts)
             {
                 if (isNumber(token))
@@ -24,19 +36,41 @@ namespace CPE200Lab1
                 else if (isOperator(token))
                 {
                     //FIXME, what if there is only one left in stack?
+                    if (rpnStack.Count <= 1)
+                    {
+                        return "E";
+                    }
                     secondOperand = rpnStack.Pop();
                     firstOperand = rpnStack.Pop();
-                    result = calculate(token, firstOperand, secondOperand, 4);
+
+                    result = calculate(token, firstOperand, secondOperand, 8);
                     if (result is "E")
                     {
                         return result;
                     }
                     rpnStack.Push(result);
+                }else if(token != "")
+                {
+                    return "E";
                 }
+                //Console.WriteLine(token);
             }
             //FIXME, what if there is more than one, or zero, items in the stack?
-            result = rpnStack.Pop();
-            return result;
+            if (rpnStack.Count == 1)
+            {
+
+                if (Convert.ToDecimal(rpnStack.Peek()).ToString() != rpnStack.Peek())
+                {
+                    return "E";
+                }
+                result = rpnStack.Pop();
+                return Convert.ToDecimal(result).ToString("0.####");
+
+            }
+            else
+            {
+                return "E";
+            }
         }
     }
 }
